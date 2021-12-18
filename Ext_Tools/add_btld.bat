cbea7ed69efd4da55d97f5d57a7ad2951a46219f
Philipp <philipp.springer@googlemail.com>
2021-12-18 15:51:42 +0100

    Add tools for patching checksum into application image

---
+++ Ext_Tools/add_btld.bat
@@ -0,0 +1,4 @@
+srec_cat.exe ..\Debug\PCF79xx_Prog_CubeIDE_CRC32.hex -Intel ^
+..\Debug\STM32F103_MSD_BOOTLOADER_sfyip.hex -Intel ^
+-o ..\Debug\PCF79xx_Prog_CubeIDE_CRC32_btld.hex -Intel
+
---
+++ Ext_Tools/add_crc32.bat
@@ -0,0 +1,6 @@
+srec_cat.exe ..\Debug\PCF79xx_Prog_CubeIDE.hex -Intel ^
+-fill 0xFF 0x08004800 0x0800FFFC ^
+-crop 0x08004800 0x0800FFFC ^
+-CRC32_Big_Endian 0x800FFFC ^
+-o ..\Debug\PCF79xx_Prog_CubeIDE_CRC32.hex -Intel
+
