8689f73d6517f16edb1bc9496ed7c3b92e4f7abe
Philipp <philipp.springer@googlemail.com>
2025-04-26 00:16:01 +0200

    Correct post-build commands and batch script

--- Ext_Tools/add_btld.bat
+++ Ext_Tools/add_btld.bat
@@ -1,4 +1,4 @@
-srec_cat.exe ..\Debug\PCF79xx_Prog_CubeIDE_CRC32.hex -Intel ^
+%1\Ext_Tools\srec_cat.exe ..\Debug\PCF79xx_Prog_CubeIDE_CRC32.hex -Intel ^
 ..\Debug\STM32F103_MSD_BOOTLOADER.hex -Intel ^
 -o ..\Debug\PCF79xx_Prog_CubeIDE_CRC32_btld.hex -Intel
 
