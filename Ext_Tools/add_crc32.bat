c7625fbbdab11a790fa00241e34b19b442b95d3f
Philipp <philipp.springer@googlemail.com>
2025-04-26 00:48:56 +0200

    Add missing files + minor changes to make project compile successfully

--- Ext_Tools/add_crc32.bat
+++ Ext_Tools/add_crc32.bat
@@ -1,5 +1,5 @@
-%1\Ext_Tools\srec_cat.exe '(' ..\Debug\PCF79xx_Prog_CubeIDE.hex -Intel ^
--fill 0xFF 0x08004000 0x0800FFFC ')' ^
+%1\Ext_Tools\srec_cat.exe  ..\Debug\PCF79xx_Prog_CubeIDE.hex -Intel ^
+-fill 0xFF 0x08004000 0x0800FFFC ^
 -crop 0x08004000 0x0800FFFC  ^
 -CRC32_Big_Endian 0x800FFFC ^
 -o ..\Debug\PCF79xx_Prog_CubeIDE_CRC32.hex -Intel
